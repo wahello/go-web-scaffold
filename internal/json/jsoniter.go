ee513913e35f586eef50445dcdb3c12e400b6a7d
nanmu42 <i@nanmu.me>
2020-10-29 10:26:07 +0800

    internal/jsoniter: use RawMessage from jsoniter directly

--- internal/json/jsoniter.go
+++ internal/json/jsoniter.go
@@ -1,8 +1,6 @@
 package json
 
 import (
-	"errors"
-
 	"github.com/json-iterator/go"
 )
 
@@ -25,21 +23,4 @@ var (
 // RawMessage is a raw encoded JSON value.
 // It implements Marshaler and Unmarshaler and can
 // be used to delay JSON decoding or precompute a JSON encoding.
-type RawMessage []byte
-
-// MarshalJSON returns m as the JSON encoding of m.
-func (m RawMessage) MarshalJSON() ([]byte, error) {
-	if m == nil {
-		return []byte("null"), nil
-	}
-	return m, nil
-}
-
-// UnmarshalJSON sets *m to a copy of data.
-func (m *RawMessage) UnmarshalJSON(data []byte) error {
-	if m == nil {
-		return errors.New("json.RawMessage: UnmarshalJSON on nil pointer")
-	}
-	*m = append((*m)[0:0], data...)
-	return nil
-}
+type RawMessage jsoniter.RawMessage
