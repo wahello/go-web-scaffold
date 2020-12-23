f71ab90c0679ee2d8d75aedd1f770151e09f3e55
nanmu42 <i@nanmu.me>
2020-12-23 12:26:52 +0800

    doc: docs on metrics

--- metric/names.go
+++ metric/names.go
@@ -1,8 +1,9 @@
 package metric
 
-// API常见的指标
+// Usual API metrics
 const (
-	RequestsTotal     = "requests.total"
+	RequestsTotal = "requests.total"
+	// RequestDuration 's unit is in seconds, per to Prometheus's suggestion
 	RequestDuration   = "request.duration"
 	RequestSizeBytes  = "request.size.bytes"
 	ResponseSizeBytes = "response.size.bytes"
